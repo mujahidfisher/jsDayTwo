dec2c2f2ed990b009db70358961cb23c0f27ff61
mujahidfisher <fishermujahid@gmail.com>
2023-05-16 14:30:54 +0200

    Exercise

---
+++ script.js/code.js
@@ -0,0 +1,16 @@
+//Q2
+
+//make and array
+// let subject = [`Python`, `C++`,`VB.Net`, `Javascript`, `Assembly`];
+// console.log(subject);
+
+//get the middle element
+// let subject = [`Python`, `C++`,`VB.Net`, `Javascript`, `Assembly`];
+// let middle = Math.floor((0 + subject.length -1) / 2);
+// console.log(subject[middle]);
+
+//get last element
+// let subject = [`Python`, `C++`,`VB.Net`, `Javascript`, `Assembly`];
+// let last = subject.pop(4);
+// console.log(last);
+
